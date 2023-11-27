dc6c8b84ba8d3e3ca24aede38a12a3b5aa4df04a
Marc Straube <email@marcstraube.de>
2023-11-27 10:39:27 +0100

    Fix libChangelogs integration.

--- scripts/init.js
+++ scripts/init.js
@@ -1,16 +1,16 @@
-libChangelogs.register(
-    "brodericks-compendium",
-    "<ul>" +
-    "<li>Big image update: All plants and monsters now use their own image or icon.</li>" +
-    "<li>Fix CR of Marsh Maw monster.</li>" +
-    "<li>Create new compendium for plant and fungi based monsters.</li>" +
-    "<li>Update desriptions of Covadish, Dagmather and Gefnul to give more information on their appearance.</li>" +
-    "<li>Add pre-defined folder structure for compendiums (FoundryVTT >= 11).</li>" +
-    "<li>Add banner images for compendiums (FoundryVTT >= 11)</li>" +
-    "</ul>",
-    "major"
-);
 Hooks.once('libChangelogsReady', function () {
+    libChangelogs.register(
+        "brodericks-compendium",
+        "<ul>" +
+        "<li>Big image update: All plants and monsters now use their own image or icon.</li>" +
+        "<li>Fix CR of Marsh Maw monster.</li>" +
+        "<li>Create new compendium for plant and fungi based monsters.</li>" +
+        "<li>Update desriptions of Covadish, Dagmather and Gefnul to give more information on their appearance.</li>" +
+        "<li>Add pre-defined folder structure for compendiums (FoundryVTT >= 11).</li>" +
+        "<li>Add banner images for compendiums (FoundryVTT >= 11)</li>" +
+        "</ul>",
+        "major"
+    );
 });
 
 Hooks.once("init", function () {
